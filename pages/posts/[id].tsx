50126f5059a09f1d235dd97defc5df5651826f9d
magdalenaz-c <zaniewska.cmagdalena@gmail.com>
2022-05-30 21:15:50 +0200

    add CREATE YOUR FIRST APP NEXT

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,31 @@
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+export async function getStaticProps({ params }: any) {
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData,
+    },
+  }
+}
+
+export async function getStatiPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export default function Post({ postData }: any) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  )
+}
